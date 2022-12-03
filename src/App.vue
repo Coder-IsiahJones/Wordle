5de771416889fd7959dce88dc03e195ba8740fe7
Isiah Jones <jones.isiah.blhs@gmail.com>
2022-12-03 14:32:13 -0500

    Init Project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import SimpleKeyboard from "./components/SimpleKeyboard.vue";
+import { onMounted } from "vue";
+
+const handleInput = (key) => {
+  console.log(key);
+};
+
+onMounted(() => {
+  window.addEventListener("keyup", (e) => {
+    e.preventDefault();
+    let key =
+      e.keyCode == 13
+        ? "{enter}"
+        : e.keyCode == 8
+        ? "{bksp}"
+        : String.fromCharCode(e.keyCode).toLowerCase();
+    handleInput(key);
+  });
+});
+</script>
+
+<template>
+  <div class="flex flex-col h-screen max-w-md mx-auto justify-evenly">
+    <simple-keyboard @onKeyPress="handleInput" />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/SimpleKeyboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="simple-keyboard"></div>
+</template>
+
+<script setup>
+import Keyboard from "simple-keyboard";
+import "simple-keyboard/build/css/index.css";
+import { ref, onMounted } from "vue";
+
+const emit = defineEmits(["onKeyPress"]);
+
+const keyboard = ref(null);
+
+const onKeyPress = (button) => {
+  emit("onKeyPress", button);
+};
+
+onMounted(() => {
+  keyboard.value = new Keyboard({
+    layout: {
+      default: [
+        "q w e r t y u i o p",
+        "a s d f g h j k l",
+        "{enter} z x c v b n m {bksp}",
+      ],
+    },
+    onKeyPress: onKeyPress,
+  });
+});
+</script>
+
+<style></style>
